81ea48b14136de619ac47c458f2bd9685caba89e
Qianfinland <qianfinland@gmail.com>
2014-12-18 08:21:51 +0200

    validate length finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
   #equal to above
   #validates(:name, presence: true)
 
-  validates :email, presence: true
+  validates :email, presence: true, length: { maximum: 255 }
 end
