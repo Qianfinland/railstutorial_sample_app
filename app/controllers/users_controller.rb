f35f83195b9a3d3282f0ce6f678fd9c2500c4f56
Qianfinland <qianfinland@gmail.com>
2015-01-06 21:31:00 +0200

    List 10.20 code fail to pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,13 @@ class UsersController < ApplicationController
     #@user = User.new(params[:user])    # Not the final implementation!
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      # Handle a successful save.
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      # log_in @user
+      # # Handle a successful save.
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
