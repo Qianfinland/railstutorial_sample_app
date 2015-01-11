49a19ffda92bad615531896dd9c9c0f627b3c84f
Qianfinland <qianfinland@gmail.com>
2015-01-11 12:11:11 +0200

    10.3 continue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'floating-springs-1012.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
